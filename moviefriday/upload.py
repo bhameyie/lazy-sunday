357eb80f6c319f8fe4d790faa07c6656205f8d7e
bhameyie <muzzled.coder@gmail.com>
2019-02-06 15:45:25 -0500

    making it possible to convert existing uploads into HSL to deal with file upload limitations:

--- moviefriday/upload.py
+++ moviefriday/upload.py
@@ -11,8 +11,9 @@ from moviefriday.auth import login_required
 from moviefriday.db import get_db
 from moviefriday.repositories import Movie, MovieRepository
 import logging
+from os import listdir
 
-ALLOWED_MOVIE_EXTENSIONS = ['mp4']
+ALLOWED_MOVIE_EXTENSIONS = ['mp4', '.mkv']
 
 bp = Blueprint('upload', __name__)
 
@@ -22,6 +23,15 @@ def uploaded_movie(movie_id):
     movie_repo = MovieRepository(get_db())
     movie = movie_repo.find_by_id(movie_id)
     return render_template('upload/movie_uploaded.html', movie=movie)
+
+
+@bp.route('/movies/upload/convert/<file_name>', methods=['POST'])
+@login_required
+def convert_stashed_file(file_name):
+    movie_title = request.form['title']
+    force_replace = True
+    filename = secure_filename(file_name)
+    return _upload_secured_movie(filename, force_replace, movie_title)
 
 
 @bp.route('/movies/upload', methods=['GET', 'POST'])
@@ -54,7 +64,14 @@ def upload_file():
             flash('File not allowed. Please ensure it is an MP4 file')
             return redirect(request.url)
 
-    return render_template('upload/movie_upload.html')
+    stashed_files = _stashed_files()
+
+    return render_template('upload/movie_upload.html', stashed_files=stashed_files)
+
+
+def _stashed_files():
+    return [f for f in listdir(current_app.config['UPLOAD_FOLDER'])
+            if _allowed_file(f) and os.path.isfile(os.path.join(current_app.config['UPLOAD_FOLDER'], f))]
 
 
 def _allowed_file(filename):
@@ -63,12 +80,16 @@ def _allowed_file(filename):
 
 
 def _upload_movie(file, force_replace, movie_title):
-    movie_repo = MovieRepository(get_db())
     filename = secure_filename(file.filename)
     filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
     file.save(filepath)
-    movie_id = ''
+    return _upload_secured_movie(filename, force_replace, movie_title)
 
+
+def _upload_secured_movie(filename, force_replace, movie_title):
+    movie_repo = MovieRepository(get_db())
+    filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+    movie_id = ''
     if 'convert' in request.form:
         model = vidconvert.make_default_req(filename, filepath)
         conversion = vidconvert.convert_mp4(model, force_replace)
@@ -83,5 +104,4 @@ def _upload_movie(file, force_replace, movie_title):
     else:
         new_movie = Movie(title=movie_title, blob_id=filename, is_mp4=True)
         movie_id = movie_repo.insert(new_movie)
-
-    return redirect(url_for('uploaded_movie', movie_id=movie_id))
+    return redirect(url_for('upload.uploaded_movie', movie_id=movie_id))
